f9d0c1bc8fd94292e2991a85be74426c077586f5
xemura <markg0s@mail.ru>
2023-07-31 16:43:34 +0300

    try Card, Box, Image

--- app/src/main/java/com/xenia/composeroomexample/MainActivity.kt
+++ app/src/main/java/com/xenia/composeroomexample/MainActivity.kt
@@ -1,39 +1,67 @@
 package com.xenia.composeroomexample
 
-import android.inputmethodservice.Keyboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row (modifier = Modifier.background(Color.Yellow).fillMaxWidth().fillMaxHeight(0.5f)) {
-                Row (modifier = Modifier.background(Color.Green).fillMaxWidth(0.5f).fillMaxHeight()) {
-                    Text(text = "hello there!")
-                    Text(text = "Text!")
-                    Text(text = "hello there!")
-                }
-                Column (modifier = Modifier.background(Color.Blue).fillMaxHeight().fillMaxWidth(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = "One!")
-                    Text(text = "Two!")
-                    Text(text = "Three!")
+            Column {
+                ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+            }
+        }
+    }
+}
+
+@Composable
+fun ListItem(name : String, prof: String) {
+    Card (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .background(Color.White),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 5.dp
+        )
+    ) {
+        Box() {
+            Row (verticalAlignment = Alignment.CenterVertically) {
+                Image (painterResource(id = R.drawable.img),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape))
+                Column(modifier = Modifier.padding(start = 16.dp)) {
+                    Text(text = name)
+                    Text(text = prof)
                 }
             }
         }
