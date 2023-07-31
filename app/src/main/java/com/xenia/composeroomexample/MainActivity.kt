af25db04ece836393d7867df65e123be17bb8fde
xemura <markg0s@mail.ru>
2023-07-31 19:11:00 +0300

    work with modifier

--- app/src/main/java/com/xenia/composeroomexample/MainActivity.kt
+++ app/src/main/java/com/xenia/composeroomexample/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.xenia.composeroomexample
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -21,18 +28,33 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
+            Column (modifier = Modifier.verticalScroll(rememberScrollState())) {
                 ListItem(name = "Naked Science", prof = "Article")
                 ListItem(name = "Naked Science", prof = "Article")
                 ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+                ListItem(name = "Naked Science", prof = "Article")
+
             }
         }
     }
@@ -44,7 +66,14 @@ fun ListItem(name : String, prof: String) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(10.dp)
-            .background(Color.White),
+            .offset(0.dp, 0.dp).pointerInput(Unit) {
+                detectDragGesturesAfterLongPress{ change, dragAmount ->
+                    Log.d("Tag", "Long click $change $dragAmount")
+                }
+            }
+            .clickable {
+                Log.d("Tag", "click")
+            },
         shape = RoundedCornerShape(15.dp),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 5.dp
