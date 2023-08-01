944b094dc0f46ef0fcc2f49d4d2adedc31920506
xemura <markg0s@mail.ru>
2023-08-01 18:28:11 +0300

    add lazy row and column

--- app/src/main/java/com/xenia/composeroomexample/MainActivity.kt
+++ app/src/main/java/com/xenia/composeroomexample/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -17,13 +21,34 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
-                ListItem()
+            LazyRow(modifier = Modifier.fillMaxSize()) {
+                itemsIndexed(listOf("Hello", "WHAT", "End"))
+                { index, item ->
+                    Text(
+                        text = "Item $index and $item",
+                        fontSize = 30.sp,
+                        modifier = Modifier.padding(horizontal = 10.dp))
+                }
+            }
+
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize().padding(50.dp)
+            ) {
+                itemsIndexed(listOf("Hello", "WHAT", "End"))
+                { index, item ->
+                    Text(
+                        text = "Item $index and $item",
+                        fontSize = 30.sp,
+                        modifier = Modifier.padding(vertical = 10.dp))
+                }
             }
         }
     }
