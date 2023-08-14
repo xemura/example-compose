4a7e4329485cda25bf6e2ad76050a25308c3ba73
xemura <markg0s@mail.ru>
2023-08-14 17:26:00 +0300

    add topBar

--- app/src/main/java/com/xenia/composeroomexample/MainActivity.kt
+++ app/src/main/java/com/xenia/composeroomexample/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
             }
             ListItem()
 
-            MainScreen()
+            MainScreen(applicationContext)
         }
     }
 }
--- app/src/main/java/com/xenia/composeroomexample/bottom_navigation/MainScreen.kt
+++ app/src/main/java/com/xenia/composeroomexample/bottom_navigation/MainScreen.kt
@@ -1,15 +1,58 @@
 package com.xenia.composeroomexample.bottom_navigation
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun MainScreen() {
+fun MainScreen(context: Context) {
     val navController = rememberNavController()
-    Scaffold(bottomBar = {BottomNavigation(navController = navController)}) {
+    Scaffold(
+        topBar =
+        {
+            TopAppBar(
+                title = {
+                    Text(text = "Menu")
+                },
+                backgroundColor = Color.White,
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            Toast.makeText(context,"menu", Toast.LENGTH_SHORT).show()
+                        }
+                    ) {
+                        Icon(imageVector = Icons.Filled.Menu, contentDescription = null)
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(imageVector = Icons.Filled.Delete, contentDescription = null)
+                    }
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(imageVector = Icons.Filled.Share, contentDescription = null)
+                    }
+                }
+            )
+        },
+        bottomBar = {BottomNavigation(navController = navController)}
+    ) {
         NavGraph(navHostController = navController)
     }
 }
